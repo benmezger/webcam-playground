bfe916cdd9bac06a562cd99e3f950aff8d6b0431
Ben Mezger <me@benmezger.nl>
2019-10-19 23:40:20 -0300

    Feat: Add validators to the VideoForm

--- src/webcam/forms.py
+++ src/webcam/forms.py
@@ -1,5 +1,17 @@
 from django import forms
+from django.core.validators import FileExtensionValidator
+
+import filetype
 
 
 class VideoForm(forms.Form):
-    video = forms.FileField()
+    video = forms.FileField(validators=(FileExtensionValidator,))
+
+    def clean_video(self):
+        data = self.cleaned_data['video']
+        kind = filetype.guess(data.read())
+        data.seek(0)
+
+        if kind is None:
+            return False
+        return kind.mime == 'video/webm'
